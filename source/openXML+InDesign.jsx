c8ebcd71203c95a2127178f2d00f133fd049e467
Miquel Rojas Cherto <inf@miquelrojascherto.com>
2017-01-04 06:55:46 +0100

    Indesign script open a InDesign template file and its corresponding XML file

---
+++ source/openXML+InDesign.jsx
@@ -0,0 +1,24 @@
+﻿//Open files
+var myXMLDoc = "BUSINESS-CARD-xsl_a",
+    myINDTDoc = "TC9980401-LAYOUT-PI2";
+
+var
+    myDate = new Date,
+    mySuffix = "_" + myDate.getYear() + myDate.getMonth() + myDate.getDate() + myDate.getHours() + myDate.getMinutes() + myDate.getSeconds(),
+    pathXMLDoc = File( "/c/Users/mique/Development/Github/XLSconverter/files/example5/"+myXMLDoc+".xml" ),
+    pathINDTDoc = File( "/c//Users/mique/Creative Cloud Files/PROJECT/INDESIGN/BUSINESS_CARD/EXAMPLE/"+myINDTDoc+".indt" ),
+    newINDDDoc = "/c/Users/mique/Creative Cloud Files/PROJECT/INDESIGN/JOBS/"+myINDTDoc+mySuffix+".indd";
+
+var newDoc = app.open( pathINDTDoc  );
+newDoc = app.activeDocument;
+
+//import xml
+newDoc.importXML( pathXMLDoc );
+
+//Save Function
+saveFiles();
+
+function saveFiles() {
+    newDoc.save( new File( newINDDDoc ) );
+    // newDoc.close();
+}
